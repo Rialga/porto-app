d176e39a86dd6d45a8cf432ffb6bace2bd24ca8e
ganipcs <muhammad.algani@pcsindonesia.co.id>
2026-02-08 18:44:15 +0700

    setup github pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import WebLayout from '@/components/layouts/WebLayout'
 import Home from '@/pages/home'
 
+
 const router = createBrowserRouter([
   {
     path: '/',
